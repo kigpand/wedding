d0411f8c559fc8edf813a0342f405aec43e30ffe
kigpand <kigpand@naver.com>
2022-06-25 22:44:13 +0900

    server 구축, page처리 완료

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const server = app.listen(3000, () => {
+    console.log('start success on port : ', 3000);
+});
+
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/First', (req, res) => {
+    res.render('first.html');
+});
+
+app.get('/Second', (req, res) => {
+    res.render('second.html');
+});
+
+app.get('/Third', (req, res) => {
+    res.render('third.html');
+});
